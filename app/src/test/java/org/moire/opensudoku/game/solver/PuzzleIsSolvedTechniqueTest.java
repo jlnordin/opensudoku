d0fbc7ce2004e528226011f143e50ee245fb15e3
Justin Nordin <jlnordin@gmail.com>
2020-03-09 00:58:46 -0700

    Add dependency to Mockito and add switch to returning default values for the mocked Android APIs. Specifically there are some static functions that are not possible to mock with Mockito but also don't affect the part of the code that needs to be tested.

--- app/src/test/java/org/moire/opensudoku/game/solver/PuzzleIsSolvedTechniqueTest.java
+++ app/src/test/java/org/moire/opensudoku/game/solver/PuzzleIsSolvedTechniqueTest.java
@@ -1,26 +1,37 @@
 package org.moire.opensudoku.game.solver;
 
-import android.test.mock.MockContext;
+import android.content.Context;
+import android.os.SystemClock;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.moire.opensudoku.game.CellCollection;
 import org.moire.opensudoku.game.SudokuGame;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+
+@ExtendWith(MockitoExtension.class)
 class PuzzleIsSolvedTechniqueTest {
 
-    @org.junit.jupiter.api.Test
+    @Mock
+    Context mContext;
+
+    @Test
     void create() {
         SudokuGame game = new SudokuGame();
         game.setCells(CellCollection.createDebugGame());
 
         AbstractTechnique technique;
-        technique = StepByStepSolver.getNextTechnique(new MockContext(), game);
+        technique = StepByStepSolver.getNextTechnique(mContext, game);
         assertFalse(technique instanceof PuzzleIsSolvedTechnique);
 
         game.solve();
 
-        technique = StepByStepSolver.getNextTechnique(new MockContext(), game);
+        technique = StepByStepSolver.getNextTechnique(mContext, game);
         assertTrue(technique instanceof PuzzleIsSolvedTechnique);
     }
 }
