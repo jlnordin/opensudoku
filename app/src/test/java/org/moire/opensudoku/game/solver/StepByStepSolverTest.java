0a37a217d30214afe053d628bf3c47d53521b3bc
Justin Nordin <jlnordin@gmail.com>
2020-03-18 02:18:15 -0700

    Add a test to check if all puzzles that come pre-packaged with OpenSudoku are solvable by the current set of hints. This is failing currently, but the number should go down with each new technique. This test also adds some console logging to categorize the techniques used in solving each puzzle to better direct the manual testing efforts.

--- app/src/test/java/org/moire/opensudoku/game/solver/StepByStepSolverTest.java
+++ app/src/test/java/org/moire/opensudoku/game/solver/StepByStepSolverTest.java
@@ -6,10 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.moire.opensudoku.db.DatabaseHelper;
+import org.moire.opensudoku.db.SudokuDatabase;
 import org.moire.opensudoku.game.Cell;
 import org.moire.opensudoku.game.CellCollection;
 import org.moire.opensudoku.game.SudokuGame;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -62,5 +65,42 @@ class StepByStepSolverTest {
         }
 
         fail("StepByStepSolver techniques do not lead to a solution.");
+    }
+
+    @Test
+    void getNextTechnique_solveAllOpenSudokuGamesWithoutBruteForce() {
+
+        int sudokusThatNeedBruteForce = 0;
+        int sudokuId = 0;
+
+        for (String sudokuString : TechniqueTestHelpers.OpenSudokuGames) {
+            SudokuGame game = new SudokuGame();
+            game.setCells(CellCollection.fromString(sudokuString));
+
+            AbstractTechnique technique;
+
+            System.out.println(String.format("Techniques for puzzle %d:", sudokuId));
+            for (int i = 0; i < 1000; i++) {
+                technique = StepByStepSolver.getNextTechnique(mContext, game);
+                assertNotNull(technique);
+                System.out.println(String.format("\t%s", technique.getClass().getName()));
+
+                if (technique instanceof PuzzleIsSolvedTechnique) {
+                    break;
+                } else if (technique instanceof BruteForceTechnique) {
+                    sudokusThatNeedBruteForce++;
+                    break;
+                }
+
+                technique.applyTechnique(game);
+            }
+
+            System.out.println();
+            sudokuId++;
+        }
+
+        System.out.println();
+        System.out.println(String.format("Puzzles that require brute force techniques: %d / %d.", sudokusThatNeedBruteForce, TechniqueTestHelpers.OpenSudokuGames.length));
+        assertEquals(0, sudokusThatNeedBruteForce);
     }
 }
